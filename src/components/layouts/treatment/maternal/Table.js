05dd852febe05e9521329b30e13cf9b8bc26df98
Laurence Bautista <rorens05@gmail.com>
2019-12-03 12:35:00 +0800

    FIx patient code

--- src/components/layouts/treatment/maternal/Table.js
+++ src/components/layouts/treatment/maternal/Table.js
@@ -62,7 +62,8 @@ export default class Table extends Component {
 				<thead>
 					<tr>
 						<th>Family No.</th>
-						<th>Patient Code</th>
+						<th>Patient Code</th> 
+						
 						<th>Patient Name</th>
 						<th>Birth Outcome</th>
 						<th>Type of Delivery</th>
